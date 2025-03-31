8ec3f9c7dc54e21175df00edbdc152436098db0c
Aditi Sachan <aditisachan2003@gmail.com>
2025-03-31 21:45:14 +1100

    changed cors services allowance to include both local and deployed vercel frontend

--- api/Program.cs
+++ api/Program.cs
@@ -9,9 +9,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("http://localhost:5173") // Change this to your frontend's URL
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
+        builder => builder.WithOrigins(
+            "http://localhost:5173",
+            "https://capstonewebsvf-5-0-aditi-grnrwej2z-aditisachans-projects.vercel.app"
+            ) // Change this to your frontend's URL
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 var app = builder.Build();
