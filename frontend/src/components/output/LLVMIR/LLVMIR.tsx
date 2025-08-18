88b3a5f6659a307d65d97d24eb34fac42b4efc88
Christian Tolentino <chriscla2003@gmail.com>
2025-08-18 17:22:46 +1000

    Remove unused directive

--- frontend/src/components/output/LLVMIR/LLVMIR.tsx
+++ frontend/src/components/output/LLVMIR/LLVMIR.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import React, { useRef, useEffect, useState } from 'react';
 import Editor, { OnMount, useMonaco } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor';
